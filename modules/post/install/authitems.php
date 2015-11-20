51adf37a930eb1e044c43dab1fe47489816dd97f
Amir Basiri <amir.basiri@gmail.com>
2015-11-20 18:55:07 +0330

    added authitems to the install folder

---
+++ modules/post/install/authitems.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'permissions' => [
+       'create'=>'ایجاد نوشته',
+       'update'=>'ویرایش نوشته',
+       'delete'=>'حذف نوشته',
+       'categories'=>'مدیریت دسته های نوشته',
+    ],
+    'roles' => [
+
+    ],
+    'relations' => [
+
+    ],
+    'ruleAssignments' => [
+
+    ],
+];
