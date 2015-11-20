4c650513eb263a3c6bb72b685077b226acb6b647
Amir Basiri <amir.basiri@gmail.com>
2015-11-20 20:23:30 +0330

    added title and menu and config to the module class

--- modules/post/backend/Module.php
+++ modules/post/backend/Module.php
@@ -4,12 +4,14 @@ namespace modules\post\backend;
 
 class Module extends \yii\base\Module
 {
+	public $title;
+    public $menu;
+	public $defaultRoute = 'manage/index';
     public $controllerNamespace = 'modules\post\backend\controllers';
 
     public function init()
     {
         parent::init();
-
-        // custom initialization code goes here
+        \Yii::configure($this, require(__DIR__ . '/config.php'));
     }
 }
