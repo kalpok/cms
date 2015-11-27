082ee5abe77d08791fb148578ce491534052596b
Amir Basiri <amir.basiri@gmail.com>
2015-11-27 11:33:21 +0330

    created the frontend module of the post. It is just for iranian and I have to refactor it later.

---
+++ modules/post/frontend/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace modules\post\frontend;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'modules\post\frontend\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ modules/post/frontend/controllers/FrontController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace modules\post\frontend\controllers;
+
+use Yii;
+use yii\web\Controller; 
+use yii\web\NotFoundHttpException;
+use modules\post\frontend\models\Post;
+use modules\post\frontend\models\PostSearch;
+use modules\post\frontend\models\Category;
+use yii\data\ActiveDataProvider;
+
+/**
+ * FrontController implements the CRUD actions for Post model.
+ */
+class FrontController extends Controller
+{
+    public $layout = '//two-column';
+
+    public function actionIndex()
+    {
+        $post = Post::find()
+            ->andWhere('isActive = 1');
+        $dataProvider = new ActiveDataProvider([
+            'query' => $post,
+        ]);
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionView($slug)
+    {
+        $post = Post::find()
+            ->where(['slug'=>$slug])
+            ->one();
+        if ($post == null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+        return $this->render('view', [
+            'model' => $post,
+        ]);
+    }
+
+    public function actionCategory($slug)
+    {
+        $category = Category::find()
+            ->andWhere(['slug' => $slug])
+            ->one();
+        if ($category == null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+        $post = Post::find()
+            ->joinWith('categories')
+            ->andWhere('categoryId = '.$category->id);
+        $dataProvider = new ActiveDataProvider([
+            'query' => $post,
+        ]);
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+}
