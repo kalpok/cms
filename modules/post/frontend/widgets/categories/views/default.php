40217e20db1fb1d0a9a4cefb76f77a93f857f773
Amir Hossein Basiri <amir.basiri@gmail.com>
2017-12-15 20:56:11 +0330

    changed the post list page to add compatibility with the bootstrap theme

--- modules/post/frontend/widgets/categories/views/default.php
+++ modules/post/frontend/widgets/categories/views/default.php
@@ -1,10 +1,17 @@
 <?php
     use yii\helpers\Html;
 ?>
-<ul>
-<?php foreach ($categories as $category) : ?>
-    <li>
-        <?= Html::a($category->title, ['post/front/category', 'slug' => $category->slug]) ?>
-    </li>
-<?php endforeach ?>
-</ul>
+<div class="well">
+    <h4><?= $this->context->title ?></h4>
+    <div class="row">
+        <div class="col-lg-12">
+			<ul class="list-unstyled">
+			<?php foreach ($categories as $category) : ?>
+			    <li>
+			        <?= Html::a($category->title, ['post/front/category', 'slug' => $category->slug]) ?>
+			    </li>
+			<?php endforeach ?>
+			</ul>
+        </div>
+    </div>
+</div>
