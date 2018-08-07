9f6b6101f36c8391a321c4d3fcf9258b9432ccd9
Amirreza <amirreza.developer@gmail.com>
2018-08-08 02:26:58 +0430

    Feature/select2 (#8)
    * add kartik select2 widget and write wrapper on it with some default configurations
    
    * use select2 widget in page
    
    * use select2 widget for selecting categories in post creation form

--- modules/post/backend/views/manage/_form.php
+++ modules/post/backend/views/manage/_form.php
@@ -3,12 +3,13 @@
 use yii\helpers\Html;
 use core\helpers\Utility;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use core\widgets\select2\Select2;
 use themes\admin360\widgets\Panel;
 use themes\admin360\widgets\Button;
 use modules\post\backend\models\Category;
 use themes\admin360\widgets\editor\Editor;
 use extensions\i18n\widgets\LanguageSelect;
-use dosamigos\selectize\SelectizeTextInput;
 use extensions\file\widgets\singleupload\SingleImageUpload;
 
 $backLink = $model->isNewRecord ? ['index'] : ['view', 'id' => $model->id];
@@ -110,25 +111,19 @@ $backLink = $model->isNewRecord ? ['index'] : ['view', 'id' => $model->id];
                 'title' => 'دسته‌ها'
             ]) ?>
                 <?= $form->field($model, 'categories')->widget(
-                        SelectizeTextInput::className(),
-                        [
-                            'options' => ['class' => 'form-control'],
-                            'clientOptions' => [
-                                'create' => false,
-                                'options' => Utility::makeReadyForSelectize(
-                                    Category::find()->all(),
-                                    'title',
-                                    'title'
-                                ),
-                                'items' => $model->getCategoriesAsArray(),
-                                'valueField' => 'title',
-                                'labelField' => 'title',
-                                'searchField' => ['title'],
-                                'plugins' => ['remove_button'],
-                            ],
+                    Select2::class,
+                    [
+                        'data' => ArrayHelper::map(
+                            Category::find()->all(),
+                            'title',
+                            'title'
+                        ),
+                        'options' => [
+                            'multiple' => true,
+                            'value' => $model->getCategoriesAsArray()
                         ]
-                    )->label('');
-                ?>
+                    ]
+                )->label('') ?>
             <?php Panel::end() ?>
             <?php Panel::begin([
                 'title' => 'ویژگی های نوشته'
