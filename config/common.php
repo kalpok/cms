7de922f4471b156bda739bb8b6a46e31f072cc85
Amirreza Yeganegi <amirreza.developer@gmail.com>
2018-08-08 11:41:37 +0430

    remove linkAssets for AssetManager component

--- config/common.php
+++ config/common.php
@@ -63,9 +63,6 @@ $config = [
             'enableAutoLogin' => true,
             'loginUrl' => ['/user/auth/login'],
             'identityClass' => 'modules\user\common\components\UserIdentity'
-        ],
-        'assetManager' => [
-            'linkAssets' => YII_ENV_DEV ? true : false
         ]
     ],
     'params' => [
