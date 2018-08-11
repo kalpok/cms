7f38defa5b93f2b350c0fbdbfa0814f1aa451615
Hesam Khaki <h.n.khaki@gmail.com>
2018-08-11 21:28:06 +0430

    enhances TitleColumn grid class to support ajax grids

--- core/grid/TitleColumn.php
+++ core/grid/TitleColumn.php
@@ -6,16 +6,20 @@ use yii\grid\DataColumn;
 
 class TitleColumn extends DataColumn
 {
+    public $isAjaxGrid = false;
+
     public function init()
     {
         if (!isset($this->attribute)) {
             $this->attribute = 'title';
         }
         $this->format = 'raw';
-        $this->value =
-        function ($model) {
+        $this->value = function ($model) {
             $attribute = $this->attribute;
-            return Html::a($model->$attribute, ['view', 'id'=>$model->id]);
+            return Html::a($model->$attribute, ['view', 'id'=>$model->id], [
+                'title' => \Yii::t('yii', 'View'),
+                'class' => $this->isAjaxGrid ? 'ajaxview' : ''
+            ]);
         };
     }
 }
