b52d52ed417d1dda09f1422d0b4e0482ae648eea
Hesam Khaki <nk.hesam@gmail.com>
2020-06-25 03:55:44 +0430

    Reverse url rules to prioritize custom local rules located in local config file

--- config/frontend.php
+++ config/frontend.php
@@ -10,15 +10,16 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            // these will get reversed in final config array
             'rules' => [
-                '' => 'site/index',
-                'file/serve-image' => 'file/serve-image',
-                'file/download/<name>' => 'file/serve-file',
-                '<module>' => '<module>/front/index',
-                '<module>/<id:\d+>' => '<module>/front/view',
-                '<module>/<id:\d+>/<title>' => '<module>/front/view',
-                '<module>/<slug>' => '<module>/front/view',
                 '<module>/category/<slug>' => '<module>/front/category',
+                '<module>/<slug>' => '<module>/front/view',
+                '<module>/<id:\d+>/<title>' => '<module>/front/view',
+                '<module>/<id:\d+>' => '<module>/front/view',
+                '<module>' => '<module>/front/index',
+                'file/download/<name>' => 'file/serve-file',
+                'file/serve-image' => 'file/serve-image',
+                '' => 'site/index',
             ]
         ]
     ],
